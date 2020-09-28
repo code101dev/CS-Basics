24ab5548d20d2f115ccab2ea86799eda86ffc437
Kiggi <mail@kbrandner.com>
2020-09-28 19:57:48 +0200

    Create Example #1.cs
    Created Example #1 for Hello World lesson

---
+++ 0 - Hello World/Example #1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
